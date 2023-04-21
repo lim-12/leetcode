a6827d7b35330600d26e6c6621842ad6c9a6473e
임현교 <lhk@wemakeprice.com>
2023-04-21 10:21:03 +0900

    Initial Commit

---
+++ LeetCode/src/test/java/com/leet/leetcode/LeetCodeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.leet.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
